7b9c580d7bb62965aaf4b1fc08a060d5dbdabd77
ksagrawal143 <ksagrawal143@gmail.com>
2021-04-22 20:22:11 +0530

    BAckend

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.set("view engine", "html")
+// app.set('views', path.join(__dirname, 'views'))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+app.listen(3000, () => {
+    console.log('Server is up on port'+3000)
+})
