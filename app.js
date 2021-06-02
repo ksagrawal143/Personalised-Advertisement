5d5fd8bbcf7b7f401be5ef13252d50cd7c9348af
Kshitij Agrawal <ksagrawal143@gmail.com>
2021-06-02 19:23:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,32 +4,29 @@ const neatCSV=require('neat-csv')
 const fs=require('fs')
 const bodyParser = require('body-parser'); 
 const app = express()
-
+// const adFetch=require('./features')
 app.set("view engine", "html")
 
 app.use(express.static(__dirname + '/'));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
   
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+let currentData={};
+app.get("/home", function (req, res) {
+    let query="?sunglasses and eyeglasses";
+    Object.keys(currentData).map(key=>{
+        query+=`${key}=${currentData[key]}&`
+    })
+    console.log(query)
+    res.redirect(`/index.html${query}`);
   });
    
   app.post("/", (req, res) => { 
-      var foo = req.body.foo; 
-      var ka = req.body.ka; 
-      console.log(foo); 
-      console.log(ka); 
-      res.send("kappa"); 
+    currentData={...req.body}  
+    console.log(req.body)
+      res.send();
   }); 
-   
-// fs.readFile('./features_detected.csv',async(err,data)=>{
-//   if(err){
-//     console.error(err)
-//     return
-//   }
-//   console.log(await neatCSV(data))
-// })
+ 
 app.listen(3000, () => {
     console.log('Server is up on port'+3000)
 })
